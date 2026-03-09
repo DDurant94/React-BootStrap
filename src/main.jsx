fc74e8888ba163ffa6b606f158230e42efe6b9f0
DDurant94 <dannyjdurant@gmail.com>
2026-03-09 12:13:36 -0500

    pages fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/React-BooStrap'>
+    <BrowserRouter>
       <App />    
     </BrowserRouter>
   </React.StrictMode>,
